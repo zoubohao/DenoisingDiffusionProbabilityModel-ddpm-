9a5151dcca8c948862fd5797dec5069b9305b9ec
ZOUbohao <bzou@nvidia.com>
2022-05-25 10:37:11 +0800

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,104 @@
+
+   
+
+import os
+from typing import Dict
+
+import torch
+import torch.optim as optim
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+from torchvision.utils import save_image
+
+from Diffusion import GaussianDiffusionSampler, GaussianDiffusionTrainer
+from Model import UNet
+from Scheduler import GradualWarmupScheduler
+
+device = torch.device('cuda:0')
+def train(modelConfig: Dict):
+    # dataset
+    dataset = CIFAR10(
+        root='./CIFAR10', train=True, download=True,
+        transform=transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]))
+    dataloader = DataLoader(dataset, batch_size=modelConfig["batch_size"], shuffle=True,num_workers=4, drop_last=True, pin_memory=True)
+
+    # model setup
+    net_model = UNet(T=modelConfig["T"], ch=modelConfig["ch"], ch_mult=modelConfig["ch_mult"], attn=modelConfig["attn"],
+        num_res_blocks=modelConfig["num_res_blocks"], dropout=modelConfig["dropout"]).to(device)
+    optimizer = torch.optim.AdamW(net_model.parameters(), lr=modelConfig["lr"], weight_decay=1e-4)
+    cosineScheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=modelConfig["epoch"], eta_min=0, last_epoch=-1)
+    warmUpScheduler = GradualWarmupScheduler(optimizer=optimizer, multiplier=5., warm_epoch=modelConfig["epoch"] // 10, after_scheduler=cosineScheduler)
+    trainer = GaussianDiffusionTrainer(net_model, modelConfig["beta_1"], modelConfig["beta_T"], modelConfig["T"]).to(device)
+
+    # start training
+    for e in range(modelConfig["epoch"]):
+        with tqdm(dataloader, dynamic_ncols=True) as tqdmDataLoader:
+            for images, labels in tqdmDataLoader:
+                # train
+                optimizer.zero_grad()
+                x_0 = images.to(device)
+                loss = trainer(x_0).sum() / 1000.
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(net_model.parameters(), modelConfig["grad_clip"])
+                optimizer.step()
+                tqdmDataLoader.set_postfix(ordered_dict={
+                    "epoch": e,
+                    "loss: ": loss.item(),
+                    "img shape: ": x_0.shape,
+                    "LR": optimizer.state_dict()['param_groups'][0]["lr"]
+                    })
+        warmUpScheduler.step()
+        torch.save(net_model.state_dict(), os.path.join(modelConfig["save_dir"], 'ckpt_' + str(e) + "_.pt"))
+
+
+def eval(modelConfig: Dict, checkpoint_name: str, sampled_img_name: str):
+    # load model and evaluate
+    with torch.no_grad():
+        device = torch.device('cuda:0')
+        model = UNet(T=modelConfig["T"], ch=modelConfig["ch"], ch_mult=modelConfig["ch_mult"], attn=modelConfig["attn"],
+        num_res_blocks=modelConfig["num_res_blocks"], dropout=0.)
+        ckpt = torch.load(os.path.join(modelConfig["save_dir"], checkpoint_name))
+        model.load_state_dict(ckpt)
+        print("model load weight done.")
+        model.eval()
+        sampler = GaussianDiffusionSampler(model, modelConfig["beta_1"], modelConfig["beta_T"], modelConfig["T"]).to(device)
+        #### Sampled from standard normal distribution
+        noisyImage = torch.randn(size=[modelConfig["batch_size"], 3, 32, 32], device=device)
+        saveNoisy = torch.clamp(noisyImage * 0.5 + 0.5, 0, 1)
+        save_image(saveNoisy, os.path.join(modelConfig["sampled_dir"],  "noisy.png"), nrow=8)
+        sampledImgs = sampler(noisyImage)
+        sampledImgs = sampledImgs * 0.5 + 0.5 # [0 ~ 1]
+        save_image(sampledImgs,os.path.join(modelConfig["sampled_dir"],  sampled_img_name), nrow=8)
+
+
+def main(state, checkpoint_name: str, sampled_img_name: str):
+    model_config = {
+        "epoch": 200,
+        "batch_size": 64,
+        "T": 1000,
+        "ch": 128,
+        "ch_mult": [1, 2, 3, 4],
+        "attn": [2],
+        "num_res_blocks": 2,
+        "dropout": 0.15,
+        "lr": 2e-4,
+        "beta_1": 1e-4,
+        "beta_T": 0.02,
+        "img_size": 32,
+        "grad_clip": 1.,
+        "save_dir": "./Checkpoints/",
+        "sampled_dir": "./SampledImgs/"
+    }
+    if state == "train":
+        train(model_config)
+    else:
+        eval(model_config, checkpoint_name, sampled_img_name)
+        
+if __name__ == '__main__':
+    main("train", "ckpt_104_.pt", "104_sampled_64.png")
